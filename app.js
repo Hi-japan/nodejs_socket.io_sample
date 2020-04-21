7da51e6fdbfc80627bfa4aca08308b7bc6da884f
Reito <orange2.stage@gmail.com>
2020-04-21 18:58:24 +0900

    改行修正、末尾のセミコロン削除

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ app.use(express.static('public'))
 let http = require('http').Server(app)
 let io = require('socket.io')(http)
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg)
-    });
-  });
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg)
+  })
+})
 
 const PORT = process.env.PORT || 3000
 
-http.listen(PORT, function(){
+http.listen(PORT, function () {
   console.log('listening on *:3000')
-});
+})
